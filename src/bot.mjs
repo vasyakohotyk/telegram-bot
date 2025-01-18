65598ea94755ad1d0da6cf20d1a09333fbc1d84d
unknown <kogotukvasil@gmail.com>
2025-01-18 12:32:09 +0200

    Initial commit

--- src/bot.mjs
+++ src/bot.mjs
@@ -2,6 +2,6 @@ import TeleBot from "telebot"
 
 const bot = new TeleBot(process.env.TELEGRAM_BOT_TOKEN)
 
-bot.on("text", msg => msg.reply.text(msg.text + 1))
+bot.on("text", msg => msg.reply.text(msg.text + msg.text + msg.text))
 
 export default bot
